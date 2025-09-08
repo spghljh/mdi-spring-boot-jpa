f6e94089b47155fc8e7c07506700a6f433b3c029
spghljh <spghljh@gmail.com>
2025-09-08 19:41:31 +0900

    250908_1941-프로젝트 생성

---
+++ src/main/java/kr/co/mdi/MdiApplication.java
@@ -0,0 +1,13 @@
+package kr.co.mdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/mdi/MdiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.mdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
